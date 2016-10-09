ba09cee085dd1124f11abefc067b6e7e2067eef9
daniil_ <daniil_evsienko@mail.ru>
2016-10-09 16:35:25 +0300

    Init commit

---
+++ Papertrail logging/Papertrail logging/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Papertrail_logging
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger("Papertrail logging");
+        static void Main(string[] args)
+        {
+            log.Debug("Did it again!");
+        }
+    }
+}
